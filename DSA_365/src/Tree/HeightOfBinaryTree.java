cb95b7dd6e2fa960bf9d5630fab552ed5db69407
Abhishek Kumar <abhisheksdot@gmail.com>
2026-02-12 22:21:16 +0530

    height of tree

---
+++ DSA_365/src/Tree/HeightOfBinaryTree.java
@@ -0,0 +1,33 @@
+package Tree;
+
+public class HeightOfBinaryTree {
+
+    public static int height(Node root) {
+        if (root == null)
+            return -1;
+
+        // compute the height of left and right subtrees
+        int lHeight = height(root.left);
+        int rHeight = height(root.right);
+
+        return Math.max(lHeight, rHeight) + 1;
+    }
+
+        public static void main(String[] args) {
+
+            // Representation of the input tree:
+            //     12
+            //    /  \
+            //   8   18
+            //  / \
+            // 5   11
+            Node root = new Node(12);
+            root.left = new Node(8);
+            root.right = new Node(18);
+            root.left.left = new Node(5);
+            root.left.right = new Node(11);
+
+            System.out.println(height(root));
+        }
+    }
+
